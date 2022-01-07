d0744be95c983a797fae34b81ea475698264da82
Bobby Schwartz <robertschwartz603@gmail.com>
2022-01-06 23:11:15 -0500

    initial work on arrays and qualifying needed values. created integer and string array variables for use in generatePassword function.

--- Main/script.js
+++ Main/script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//establishing array values
+var numbers = Array.from(Array(10).keys());
+var alpha = Array.from(Array(26)).map((e, i) => i + 65);
+var alphabet = alpha.map((x) => String.fromCharCode(x));
+var lowercase = alphabet.toString().toLowerCase();
+var special = ["!","","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","|","{","}","~"];
+
+
+//logging array variable values
+console.log(numbers);
+console.log(alphabet);
+console.log(lowercase);
+console.log(special);
+
+//password generator function
+
+function generatePassword() {
+  //WRITE THE GENERATOR HERE
+}
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
